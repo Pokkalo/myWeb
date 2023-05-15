ae0c68167818046516739a561eb6e60addb144f3
Pokkalo <89689544+Pokkalo@users.noreply.github.com>
2023-05-16 00:40:46 +0800

    gd

--- src/components/Gallary.js
+++ src/components/Gallary.js
@@ -8,14 +8,14 @@ const Gallary = () => {
 
         <h1 class="--gallary-title">masonry image gallary</h1>
         <div class="--gallary-image-container">
-            <img src={usedImgsForFYP[0].url} alt=""/>
+            {/* <img src={usedImgsForFYP[0].url} alt=""/>
             <img src={require("../imgs/gallary/2.jpeg")} alt=""/>
             <img src={require("../imgs/gallary/3.jpeg")} alt=""/>
             <img src={require("../imgs/gallary/4.jpeg")} alt=""/>
             <img src={require("../imgs/gallary/5.jpeg")} alt=""/>
             <img src={require("../imgs/gallary/6.jpeg")} alt=""/>
             <img src={require("../imgs/gallary/7.jpeg")} alt=""/>
-            <img src={require("../imgs/gallary/8.jpeg")} alt=""/>
+            <img src={require("../imgs/gallary/8.jpeg")} alt=""/> */}
 
             {usedImgsForFYP.map((img)=>
               {
--- src/data/YourImg.js
+++ src/data/YourImg.js
@@ -7,35 +7,35 @@ export const usedImgsForFYP = [
     
     {
         id:1,
-        url:'../imgs/gallary/1.jpeg',
+        url: require('../imgs/gallary/1.jpeg'),
     },
     {
         id:2,
-        url:'../imgs/gallary/2.jpeg',
+        url: require('../imgs/gallary/2.jpeg'),
     },
     {
         id:3,
-        url:'../imgs/gallary/3.jpeg',
+        url: require('../imgs/gallary/3.jpeg'),
     },
     {
         id:4,
-        url:'../imgs/gallary/4.jpeg',
+        url: require('../imgs/gallary/4.jpeg'),
     },
     {
         id:5,
-        url:'../imgs/gallary/5.jpeg',
+        url: require('../imgs/gallary/5.jpeg'),
     },
     {
         id:6,
-        url:'../imgs/gallary/6.jpeg',
+        url: require('../imgs/gallary/6.jpeg'),
     },
     {
         id:7,
-        url:'../imgs/gallary/7.jpeg',
+        url: require('../imgs/gallary/7.jpeg'),
     },
     {
         id:8,
-        url:'../imgs/gallary/8.jpeg',
+        url: require('../imgs/gallary/8.jpeg'),
     },
 
 
